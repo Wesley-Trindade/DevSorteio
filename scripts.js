0e0e9261a354451ad7a742d2f1bfdfefd2c68b2f
Wesley-Trindade <wesleydavid015@gmail.com>
2024-07-09 21:01:49 -0300

    caixa com valor sorteado ok

--- scripts.js
+++ scripts.js
@@ -1,22 +1,34 @@
 const buttonSortear  = document.querySelector(".button__sortear")
+// const valueResult = document.querySelector(".drawn")
+
 
 
 
    function sorteador(){
       const inputMim = Math.ceil(document.querySelector(".input__min").value)
       const inputMax = Math.floor(document.querySelector(".input__max").value)
-      const result = Math.floor(Math.random() * (inputMax - inputMim + 1) + inputMim);
-
-     
-
-  
-     
-   
-
+      const valueResult = document.querySelector(".drawn")
+      const caixaBox = document.querySelector(".value__drawn")
       
       
+      const result = Math.floor(Math.random() * (inputMax - inputMim + 1) + inputMim); 
+      
+      
+
+       valueResult.innerHTML = result
+
+       caixaBox.style.display = 'block';
+
+
+      
+
+    
 
 }
+
+
+
+
 
 
 
